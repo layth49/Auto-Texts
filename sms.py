84279c5e603324bcaa7b3e1d8a964fa39ebe575d
49 <laythandamin@gmail.com>
2025-02-13 08:03:36 -0600

    Integrating speech recognition for recipient selection in SMS functionality

--- sms.py
+++ sms.py
@@ -2,50 +2,71 @@ import os
 import subprocess
 import pyautogui
 import time
+import azure.cognitiveservices.speech as speechsdk
 
 # Define your contacts here
 contacts = {
+
+
 }
 
 
-# Collect input for recipient
-recipient_name = input("Who would you like to message? ")
+speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+speech_config.speech_recognition_language="en-US"
 
-# Check if the recipient is in your contact list
-if recipient_name not in contacts:
-    print(f"Error: {recipient_name} is not in your contact list.")
-    exit()
+audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-recipient_number = contacts[recipient_name]
-
-# Ask the user for the message
-message_body = input("What would you like to say? ")
+# Add phrases to the grammar to be recognized by the speech recognizer
+phrase_list = speechsdk.PhraseListGrammar.from_recognizer(speech_recognizer)
+phrases = list(contacts.keys())
 
 
-# Open the Phone Link app
-os.system("start explorer shell:appsfolder\\Microsoft.YourPhone_8wekyb3d8bbwe!App")
+for phrase in phrases:
+    phrase_list.addPhrase(phrase)
+    print(f"Added phrase: {phrase}")
 
-# Press the compose button
-while True:
-    try:
-        button = pyautogui.locateOnScreen("assets/compose.png", grayscale= True, confidence= 0.9)
-        pyautogui.click(button)
 
-        print("Compose button found")
-        time.sleep(1)
-        break
-    except:
-        time.sleep(1)
-        print("Waiting for the compose button to appear")
+print("Speak into your microphone.")
+speech_recognition_result = speech_recognizer.recognize_once_async().get()
 
-# Type the recipient's number
-pyautogui.write(recipient_number)
-time.sleep(0.1)
-pyautogui.press("enter")
 
-time.sleep(1)
 
-# Type the message
-pyautogui.write(message_body)
-time.sleep(0.1)
-pyautogui.press("enter")
+# This is wrong but I'll fix it later
+if contacts in speech_recognition_result:
+    
+    print("Recognized: " + speech_recognition_result.text)
+
+    recipient_number = contacts[speech_recognition_result]
+
+    # Ask the user for the message (this will be replaced by speech recognition later)
+    message_body = input("What would you like to say? ")
+
+
+    # Open the Phone Link app
+    os.system("start explorer shell:appsfolder\\Microsoft.YourPhone_8wekyb3d8bbwe!App")
+
+    # Press the compose button
+    while True:
+        try:
+            button = pyautogui.locateOnScreen("assets/compose.png", grayscale= True, confidence= 0.9)
+            pyautogui.click(button)
+
+            print("Compose button found")
+            time.sleep(1)
+            break
+        except:
+            time.sleep(1)
+            print("Waiting for the compose button to appear")
+
+    # Type the recipient's number
+    pyautogui.write(recipient_number)
+    time.sleep(0.1)
+    pyautogui.press("enter")
+
+    time.sleep(1)
+
+    # Type the message
+    pyautogui.write(message_body)
+    time.sleep(0.1)
+    pyautogui.press("enter")
